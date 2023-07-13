cfa6b830f9fad24111e05a38c15e93ba3f916c68
Martin Bobbio <martinbobbio1@gmail.com>
2023-07-13 11:28:34 -0300

    add: tailwind configuration

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,10 +1,12 @@
+import './Header.scss';
+
 /**
  * Functional component that render component header.
  *
  * @return React.ReactElement <Header/>
  */
 const Header = () => {
-  return <div>header</div>;
+  return <div className='header p-4'>header</div>;
 };
 
 export default Header;
--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import { fileURLToPath, URL } from 'url';
+import tailwind from 'tailwindcss';
 import dotenv from 'dotenv';
 
 export default defineConfig(() => {
@@ -29,6 +30,9 @@ export default defineConfig(() => {
         scss: {
           additionalData: `@import "./src/variables.scss";`,
         },
+      },
+      postcss: {
+        plugins: [tailwind],
       },
     },
   };
